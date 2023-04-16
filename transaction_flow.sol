42668b43661fbd88ae0be53fbe442627fce0290a
chelikpapa <chelikpapa@hotmail.com>
2023-04-16 14:00:00 +0200

    Integrate zk-rollups for scaling

--- transaction_flow.sol
+++ transaction_flow.sol
@@ -1,3 +1,4 @@
 // Add decentralized contract upgradeability
 // Add new feature: on-chain governance
 // Refactor contract to use EIP-712 signatures
+// Add support for decentralized credit scoring
