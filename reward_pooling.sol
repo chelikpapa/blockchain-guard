3b5652be0dd4290589a7c6d91a12788f4cae2475
chelikpapa <chelikpapa@hotmail.com>
2023-06-03 14:00:00 +0200

    Add support for decentralized credit scoring

--- reward_pooling.sol
+++ reward_pooling.sol
@@ -2,3 +2,4 @@
 // Add multi-sig wallet functionality
 // Fix interoperability issues with other blockchains
 // Add support for decentralized file storage
+// Fix interoperability issues with other blockchains
