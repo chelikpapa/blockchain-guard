e115d8c0e814a8d6de136b58c680a578b9b05ee2
chelikpapa <chelikpapa@hotmail.com>
2023-07-18 14:00:02 +0200

    Refactor staking logic

--- reward_escrow.sol
+++ reward_escrow.sol
@@ -1,3 +1,4 @@
 // Improve on-chain governance systems
 // Fix security vulnerability in random number generation
 // Optimize contract upgrade process
+// Implement adaptive block size for Ethereum
