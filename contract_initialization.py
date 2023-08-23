604a23c98a4e30f36f58e75c85719612b3e1be10
chelikpapa <chelikpapa@hotmail.com>
2023-08-23 14:00:01 +0200

    Refactor storage layout for better efficiency

--- contract_initialization.py
+++ contract_initialization.py
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Fix governance proposal approval logic
 // Fix issues with cross-chain transaction finality
+// Refactor cross-chain communication layer
