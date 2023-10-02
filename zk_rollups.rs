d8c76413912c7293ebba80d03879c144bbc140f5
chelikpapa <chelikpapa@hotmail.com>
2023-10-02 14:00:01 +0200

    Enhance logging for better debugging

--- zk_rollups.rs
+++ zk_rollups.rs
@@ -1,3 +1,4 @@
 // Add support for decentralized file storage
 // Fix interoperability issues with other blockchains
 // Enhance contract upgrade process
+// Improve decentralized lending contract flexibility
