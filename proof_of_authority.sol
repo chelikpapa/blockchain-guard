bafd97168c51e4288782313981c169ef320f4a2d
chelikpapa <chelikpapa@hotmail.com>
2023-11-01 14:00:00 +0200

    Add slashing mechanism for malicious actors

--- proof_of_authority.sol
+++ proof_of_authority.sol
@@ -2,3 +2,4 @@
 // Refactor storage layout for better efficiency
 // Add support for zk-SNARKs in smart contracts
 // Improve gas fee estimation accuracy
+// Fix security vulnerability in random number generation
