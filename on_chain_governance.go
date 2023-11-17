291ffc5c0d9a85db304b27d95bc431ef0d016000
chelikpapa <chelikpapa@hotmail.com>
2023-11-17 14:00:00 +0200

    Fix transaction replay issue

--- on_chain_governance.go
+++ on_chain_governance.go
@@ -2,3 +2,4 @@
 // Add support for multiple consensus mechanisms
 // Improve security in decentralized applications
 // Integrate with decentralized storage systems
+// Add multi-chain governance support
