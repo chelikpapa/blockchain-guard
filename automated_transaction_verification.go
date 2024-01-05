109567d81d61a437458d77f05136eaf7c93ccae0
chelikpapa <chelikpapa@hotmail.com>
2024-01-05 14:00:01 +0200

    Add new RPC endpoint

--- automated_transaction_verification.go
+++ automated_transaction_verification.go
@@ -1,3 +1,4 @@
 // Integrate automated market maker (AMM) logic
 // Integrate privacy-enhancing smart contracts
 // Add fallback mechanism in Solidity contract
+// Integrate Chainlink VRF for randomness
