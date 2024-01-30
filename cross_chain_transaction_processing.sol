17cad30308feddf67258c1765977eed47c41ae9d
chelikpapa <chelikpapa@hotmail.com>
2024-01-30 14:00:00 +0200

    Integrate decentralized prediction models

--- cross_chain_transaction_processing.sol
+++ cross_chain_transaction_processing.sol
@@ -2,3 +2,4 @@
 // Improve error handling in transaction flow
 // Add support for delegation in governance
 // Fix type mismatch in Haskell smart contract
+// Improve error handling in transaction flow
