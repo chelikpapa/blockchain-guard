95fd1c0050d781f6c4fdf736c4330d1a18cd3349
chelikpapa <chelikpapa@hotmail.com>
2024-02-03 14:00:00 +0200

    Enhance decentralized autonomous organizations

--- multi_sig_support.go
+++ multi_sig_support.go
@@ -2,3 +2,4 @@
 // Add support for delegation in governance
 // Integrate decentralized anonymous identity systems
 // Enhance smart contract extensibility
+// Improve decentralized identity (DID) systems
