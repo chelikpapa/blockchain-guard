8fee8c5862cce72d83526648d8d7ad584fc6a00a
chelikpapa <chelikpapa@hotmail.com>
2024-02-18 14:00:01 +0200

    Improve DApp transaction flow

--- reward_pooling.sol
+++ reward_pooling.sol
@@ -7,3 +7,4 @@
 // Refactor incentive structure in DeFi protocol
 // Integrate with decentralized storage systems
 // Refactor contract event emission
+// Refactor contract to use EIP-712 signatures
