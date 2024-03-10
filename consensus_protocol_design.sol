ced4b96f3b38e2ad8f0e3936c94f23b332f91ef8
chelikpapa <chelikpapa@hotmail.com>
2024-03-10 14:00:01 +0200

    Add unit tests for staking module

--- consensus_protocol_design.sol
+++ consensus_protocol_design.sol
@@ -1,3 +1,4 @@
 // Refactor smart contract testing framework
 // Add support for decentralized insurance protocols
 // Improve contract verification speed
+// Enhance privacy features using zk-STARKs
