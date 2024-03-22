a2d3fe563de269cb5b90111609ba5d536c318022
chelikpapa <chelikpapa@hotmail.com>
2024-03-22 14:00:02 +0200

    Refactor token economics model

--- zero_knowledge_rollups.go
+++ zero_knowledge_rollups.go
@@ -2,3 +2,4 @@
 // Optimize state channels for faster transactions
 // Add support for decentralized marketplaces
 // Improve smart contract code modularity
+// Implement time-locked contract execution
