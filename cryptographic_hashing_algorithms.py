07a0c674458b773feafc3df354f094e013aa452a
chelikpapa <chelikpapa@hotmail.com>
2024-03-30 14:00:00 +0200

    Add support for ERC-721A tokens

--- cryptographic_hashing_algorithms.py
+++ cryptographic_hashing_algorithms.py
@@ -1,3 +1,4 @@
 // Fix issue with multi-chain transactions
 // Improve scalability for smart contract platforms
 // Fix issues with token transfer confirmations
+// Fix scalability issues with smart contracts
