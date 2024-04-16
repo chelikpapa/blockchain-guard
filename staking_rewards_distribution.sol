75cfffc6ac7876773c11703c4ef58ffec5d64899
chelikpapa <chelikpapa@hotmail.com>
2024-04-16 14:00:00 +0200

    Fix governance proposal approval logic

--- staking_rewards_distribution.sol
+++ staking_rewards_distribution.sol
@@ -1,3 +1,4 @@
 // Fix security vulnerability in random number generation
 // Add support for multiple consensus mechanisms
 // Fix issue with multi-chain transactions
+// Refactor incentive structure in DeFi protocol
