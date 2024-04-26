18d9fdd519575a12f1749c1fb05aa96cc1433c3a
chelikpapa <chelikpapa@hotmail.com>
2024-04-26 14:00:01 +0200

    Refactor reward pooling algorithm

--- token_burn_mechanism.sol
+++ token_burn_mechanism.sol
@@ -1,3 +1,4 @@
 // Optimize state channels for faster transactions
 // Fix issues with gas optimization in smart contracts
 // Improve ERC-721 token metadata functionality
+// Fix token transfer approval issues
