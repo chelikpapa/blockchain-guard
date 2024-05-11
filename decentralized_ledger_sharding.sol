14ff048aff395b6d9b60592faed70d43aedc8bf7
chelikpapa <chelikpapa@hotmail.com>
2024-05-11 14:00:01 +0200

    Refactor staking logic

--- decentralized_ledger_sharding.sol
+++ decentralized_ledger_sharding.sol
@@ -1,3 +1,4 @@
 // Fix ERC-721 contract issues with transfers
 // Add support for zk-SNARKs in smart contracts
 // Refactor decentralized exchange (DEX) code
+// Improve staking pool reward management
