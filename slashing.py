fc7eb749b9e968a8ce8c5a390facaece8b1c8e0e
chelikpapa <chelikpapa@hotmail.com>
2024-06-29 14:00:00 +0200

    Improve decentralized exchange (DEX) liquidity

--- slashing.py
+++ slashing.py
@@ -2,3 +2,4 @@
 // Optimize gas usage for contract deployment
 // Enhance privacy features using zk-STARKs
 // Add support for zk-SNARKs in smart contracts
+// Fix issues with token vesting contracts
