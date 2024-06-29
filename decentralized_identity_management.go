f7075a2900ea143bf9c83d8321f53a2d203b563e
chelikpapa <chelikpapa@hotmail.com>
2024-06-29 14:00:02 +0200

    Improve error handling in transaction flow

--- decentralized_identity_management.go
+++ decentralized_identity_management.go
@@ -3,3 +3,4 @@
 // Improve on-chain governance systems
 // Fix issues with ERC-20 token approvals
 // Fix interoperability issues with other blockchains
+// Refactor contract to use EIP-712 signatures
