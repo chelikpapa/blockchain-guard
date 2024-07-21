23a6a13bcfc23fb93d3948559148509f5e78d396
chelikpapa <chelikpapa@hotmail.com>
2024-07-21 14:00:00 +0200

    Refactor cross-chain communication layer

--- secure_key_management.go
+++ secure_key_management.go
@@ -5,3 +5,4 @@
 // Fix overflow bug in token calculation
 // Improve error handling in transaction flow
 // Add support for decentralized lending platforms
+// Improve decentralized identity (DID) systems
