05b95d87d325ed2f462d67d0131782972003dd5e
chelikpapa <chelikpapa@hotmail.com>
2024-07-28 14:00:02 +0200

    Optimize contract storage for gas reduction

--- blockchain_identity_management.rs
+++ blockchain_identity_management.rs
@@ -2,3 +2,4 @@
 // Add support for multiple consensus mechanisms
 // Refactor reward pooling algorithm
 // Integrate with decentralized identity providers
+// Refactor storage layout for better efficiency
