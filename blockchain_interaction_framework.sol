6201cbe151e718e3058f5cec0dfdf83ae1f78254
chelikpapa <chelikpapa@hotmail.com>
2024-08-07 14:00:02 +0200

    Refactor storage layout for better efficiency

--- blockchain_interaction_framework.sol
+++ blockchain_interaction_framework.sol
@@ -1,3 +1,4 @@
 // Add new feature: on-chain governance
 // Fix type mismatch in Haskell smart contract
 // Improve staking pool reward management
+// Fix vulnerability in decentralized finance protocols
