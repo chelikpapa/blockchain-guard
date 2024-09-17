111cfded09c09d08f823ab4c3267eb07630e1e66
chelikpapa <chelikpapa@hotmail.com>
2024-09-17 14:00:01 +0200

    Integrate decentralized prediction models

--- blockchain_interaction_framework.py
+++ blockchain_interaction_framework.py
@@ -2,3 +2,4 @@
 // Fix security vulnerability in random number generation
 // Add decentralized contract upgradeability
 // Enhance logging for better debugging
+// Bugfix: incorrect reward calculation
