acf6a0f4e1cb0e0e4ff9287ad221d5ce1fab1b27
chelikpapa <chelikpapa@hotmail.com>
2024-09-27 14:00:00 +0200

    Fix compatibility issues with major wallets

--- peer_to_peer_crowdfunding.sol
+++ peer_to_peer_crowdfunding.sol
@@ -3,3 +3,4 @@
 // Enhance logging for better debugging
 // Integrate with Ethereum Layer-2 solutions
 // Fix oracle data delays in decentralized apps
+// Add support for zk-SNARKs in smart contracts
