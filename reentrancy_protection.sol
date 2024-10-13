dfbd00baa41653d99432fd8e7c9d153186735337
chelikpapa <chelikpapa@hotmail.com>
2024-10-13 14:00:01 +0200

    Add support for delegation in governance

--- reentrancy_protection.sol
+++ reentrancy_protection.sol
@@ -1,3 +1,4 @@
 // Fix wallet interaction issues in DeFi protocols
 // Add support for decentralized lending platforms
 // Integrate automated market maker (AMM) logic
+// Enhance decentralized autonomous governance
