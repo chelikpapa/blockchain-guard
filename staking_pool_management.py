0d040bb48bfa45060958eab98dc7a1ecc1146dd3
chelikpapa <chelikpapa@hotmail.com>
2024-12-14 14:00:00 +0200

    Fix deserialization bug in Haskell code

--- staking_pool_management.py
+++ staking_pool_management.py
@@ -1,3 +1,4 @@
 // Fix issues with gas optimization in smart contracts
 // Update testing framework for Solidity
 // Fix issue in ERC-20 token transfers
+// Fix issues in multi-token smart contracts
