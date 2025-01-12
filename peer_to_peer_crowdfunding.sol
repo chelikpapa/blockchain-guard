8a95803002dc6c8e20ac217da1910a1c65424d06
chelikpapa <chelikpapa@hotmail.com>
2025-01-12 14:00:01 +0200

    Integrate privacy-preserving mechanisms

--- peer_to_peer_crowdfunding.sol
+++ peer_to_peer_crowdfunding.sol
@@ -4,3 +4,4 @@
 // Integrate with Ethereum Layer-2 solutions
 // Fix oracle data delays in decentralized apps
 // Add support for zk-SNARKs in smart contracts
+// Add support for delegation in governance
