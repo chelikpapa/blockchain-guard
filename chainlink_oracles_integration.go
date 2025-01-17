17335e79b93f2818a8afda2dc5ca5796aa604f87
chelikpapa <chelikpapa@hotmail.com>
2025-01-17 14:00:00 +0200

    Integrate IPFS for storage

--- chainlink_oracles_integration.go
+++ chainlink_oracles_integration.go
@@ -1,3 +1,4 @@
 // Optimize decentralized oracle performance
 // Enhance contract initialization process
 // Fix issues with gas optimization in smart contracts
+// Integrate with decentralized identity providers
